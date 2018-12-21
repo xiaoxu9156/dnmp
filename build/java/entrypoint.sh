2048c927f716f4740c5d167ec0cd291648bb0657
xiaoxu <zxx9156@qq.com>
2018-12-22 00:49:06 +0800

    新增java镜像

---
+++ build/java/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+set -x
+
+lib=/app
+classpath=.
+
+for file in ${lib}/*.jar;
+    do classpath=${classpath}:$file;
+done
+
+# LAJP服务启动指令(前台)
+java -classpath $classpath lajpsocket.PhpJava
